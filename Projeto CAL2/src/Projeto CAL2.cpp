9b0fe99bb0d366bd898bbf6beb84d27ca98da9dd
leonardo-faria <leonardofaria94@gmail.com>
2014-06-01 19:45:58 +0100

    choose word and size done

--- Projeto CAL2/src/Projeto CAL2.cpp
+++ Projeto CAL2/src/Projeto CAL2.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#include <limits.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 #include "time.h"
-#include <vector>
-#include <sstream>
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 typedef struct score {
@@ -15,10 +24,14 @@ vector<string> read(string file, int size) {
 	ifstream myfile(file.c_str());
 	string line;
 	if (myfile.is_open()) {
-		while (getline(myfile, line, ' ')) {
-			if ((int) line.size() == size)
-				v.push_back(line);
-			getline(myfile, line);
+		while (getline(myfile, line)) {
+			int i;
+			for (i = 0; i < (int) line.size(); ++i) {
+				if (line[i] == ' ')
+					break;
+			}
+			if (i == size)
+				v.push_back(line.substr(0, i));
 		}
 		myfile.close();
 	}
@@ -55,32 +68,113 @@ score get_score(string code, string guess) {
 	return s;
 }
 
+int exact(string s1, string s2) {
+	if (s1.size() > s2.size())
+		return 0;
+	for (int i = 0; i < (int) s1.size(); ++i) {
+		if (s1[i] != s2[i])
+			return 0;
+	}
+	if (s1.size() == s2.size())
+		return 1;
+	return 2;
+}
+
+int getch(void) {
+	struct termios oldattr, newattr;
+	int ch;
+	tcgetattr( STDIN_FILENO, &oldattr);
+	newattr = oldattr;
+	newattr.c_lflag &= ~( ICANON | ECHO);
+	tcsetattr( STDIN_FILENO, TCSANOW, &newattr);
+	ch = getchar();
+	tcsetattr( STDIN_FILENO, TCSANOW, &oldattr);
+	cout << toupper(ch);
+	return ch;
+}
+void clear() {
+	printf("\033[2J\033[1;1H"); //console cleaner
+}
+
+string choose(vector<string> S) {
+	string str = "";
+	while (1) {
+		int matches = 0, printed = 0;
+		int r;
+		clear();
+		cout << "Please choose a word\n";
+		cout << str << endl;
+		cout << "Matches:" << endl;
+		for (int i = 0; i < (int) S.size(); i++) {
+			if ((r = exact(str, S[i])) != 0)
+				if (printed < 15) {
+					cout << S[i] << "\n";
+					printed++;
+				} else if (printed == 15) {
+					cout << "...\n";
+					printed++;
+				}
+			if (r == 1)
+				matches++;
+		}
+		int ch = toupper(getch());
+		if (ch >= 'A' && ch <= 'Z')
+			str += (char) ch;
+		else if (ch == 127 && str.size() > 0)
+			str.pop_back();
+		else if (ch == 10) {
+			if (matches > 0)
+				break;
+			else {
+				clear();
+				cout << str << " does not match any word in the storage!\nPress any key to continue\n";
+				getch();
+			}
+		}
+	}
+	return str;
+}
+
 int main() {
+	clear();
 	time_t begin, end;
-	time(&begin);
 	score s;
 	int n = 1;
-	string code = "GRUDGER";
+	string code;
 	string guess;
-	int size = code.size();
+	int size;
+	while(1)
+	{
+		cout << "What size do you want to play with?" << endl;
+		cin >> size;
+		getch();
+		clear();
+		if(size<2 || size >15)
+			cout << "Only words with 2 to 15 letters available"<< endl;
+		else break;
+	}
 	vector<string> S = read("words.txt", size);
+	code=choose(S);
+	time(&begin);
+	clear();
 	guess = S[0];
-	s =get_score(code, guess);
+	s = get_score(code, guess);
 	while (s.colored != size) {
+		cout << "New Guess: " << guess << "\nScore: " << s.colored << " colored," << s.whites << " white" << endl;
+		vector<string> temp;
 		for (int i = 0; i < (int) S.size(); ++i) {
 			score gs;
 			gs = get_score(guess, S[i]);
-			if (gs.colored != s.colored || gs.whites != s.whites) {
-				S.erase(S.begin() + i);
-				--i;
-			}
+			if (gs.colored == s.colored && gs.whites == s.whites)
+				temp.push_back(S[i]);
 		}
+		S = temp;
 		guess = S[0];
 		s = get_score(code, guess);
 		++n;
-	}
 
+	}
+	cout << "New Guess: " << guess << "\nScore: " << s.colored << " colored," << s.whites << " white" << endl;
 	time(&end);
-	cout << "Solved code " << guess << " in " << n << " steps ("
-			<< difftime(end, begin) << " seconds)." << endl;
+	cout << "Solved code " << guess << " in " << n << " steps (" << difftime(end, begin) << " seconds)." << endl;
 }
